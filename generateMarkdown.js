9f592135b8d866cb2d7079bff330a6b3bc465701
haartmuhn <haartmuhn@gmail.com>
2024-05-02 22:37:35 -0700

    Added JS code to application

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,80 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render the license badge based on the provided license type
+function renderLicenseBadge(license) {
+  if (license) {
+    // If a license is provided, return a markdown string for the license badge with the corresponding URL
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  } else {
+    // If no license is provided, return an empty string
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the URL of the license based on the provided license type
+function renderLicenseLink(license) {
+  if (license) {
+    // If a license is provided, return the URL of the license
+    return `https://opensource.org/licenses/${license}`;
+  } else {
+    // If no license is provided, return an empty string
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render the license section of the markdown file
+function renderLicenseSection(license) {
+  if (license) {
+    // If a license is provided, return a markdown string for the license section with a link to the license URL
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the [${license} License](${renderLicenseLink(license)}).`;
+  } else {
+    // If no license is provided, return an empty string
+    return '';
+  }
+}
+
+// Main function to generate the markdown file content
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions about this project, please reach out to [${data.author}](https://github.com/${data.username}) at ${data.email}.
 `;
 }
 
+// Export the generateMarkdown function to be used by other modules
 module.exports = generateMarkdown;
